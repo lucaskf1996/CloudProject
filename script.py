d3f549b1b284ee084cc8344312acdb6a87b5f67e
lucaskf1996 <lucaskf1996@gmail.com>
2021-11-22 20:31:01 -0300

    delete commentaries

--- script.py
+++ script.py
@@ -6,96 +6,6 @@ from botocore.exceptions import ClientError
 from time import sleep
 import time
 
-# with open('credentials.json') as fd:
-#     credentials = json.load(fd)
-
-# TYPE_DB = "DB"
-# TYPE_WB = "WEB"
-
-# AWSACCESSKEYID = credentials["aws_access_key_id"]
-# AWSSECRETACCESSKEY = credentials["aws_secret_access_key"]
-
-# #Trocar nome
-# OWNER_NAME = "my-deploy"
-# KEY_PAIR_NAME = "my-deploy-key"
-# SEC_GROUP_NAME = "my-deploy-sg"
-# POSTGRES_PW = "cloud"
-# POSTGRES_IP = ""
-# POSTGRES_ID = ""
-
-# DJANGO_IP = "" 
-# DJANGO_ID = ""
-
-# OWNER_NAME_NV = OWNER_NAME+"_NV"
-# KEY_PAIR_NAME_NV = KEY_PAIR_NAME+"_NV"
-# SEC_GROUP_NAME_NV = SEC_GROUP_NAME+"_NV"
-# SEC_GROUP_ID_NV = ""
-
-# OWNER_NAME_OH = OWNER_NAME+"_OH"
-# KEY_PAIR_NAME_OH = KEY_PAIR_NAME+"_OH"
-# SEC_GROUP_NAME_OH = SEC_GROUP_NAME+"_OH"
-# SEC_GROUP_ID_OH = ""
-
-# PERMISSION_DJ =[
-#     {'IpProtocol': 'tcp',
-#     'FromPort': 22,
-#     'ToPort': 22,
-#     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-#     {'IpProtocol': 'tcp',
-#     'FromPort': 8080,
-#     'ToPort': 8080,
-#     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-# ]
-
-# SEC_GROUP_NAME_DB = SEC_GROUP_NAME+"_DB"
-# SEC_GROUP_ID_DB = ""
-
-# PERMISSION_DB = [
-#     {'IpProtocol': 'tcp',
-#     'FromPort': 22,
-#     'ToPort': 22,
-#     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-#     {'IpProtocol': 'tcp',
-#     'FromPort': 5432,
-#     'ToPort': 5432,
-#     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-# ]
-
-
-# SEC_GROUP_NAME_LB = SEC_GROUP_NAME+"_LB"
-# SEC_GROUP_ID_LB = ""
-# LB_ARN =""
-
-# PERMISSION_LB = [
-#     {'IpProtocol': 'tcp',
-#     'FromPort': 80,
-#     'ToPort': 80,
-#     'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-# ]
-
-# TARGETGROUP_NAME = OWNER_NAME + "-TG"
-# TG_ARN =""
-
-# AUTOSCALE_NAME = OWNER_NAME + "_AS"
-
-# LAUNCH_NAME = OWNER_NAME + "_LAUNCH"
-
-# #UBUNTU para cada regiao
-# UBUNTU_OH = "ami-0b9064170e32bde34"
-# UBUNTU_NV = "ami-0747bdcabd34c712a"
-
-# client_nv = boto3.client("ec2", region_name="us-east-1", aws_access_key_id=AWSACCESSKEYID, aws_secret_access_key=AWSSECRETACCESSKEY)
-# client_oh = boto3.client("ec2", region_name="us-east-2", aws_access_key_id=AWSACCESSKEYID, aws_secret_access_key=AWSSECRETACCESSKEY)
-# clientLB  = boto3.client('elbv2', region_name="us-east-1", aws_access_key_id=AWSACCESSKEYID, aws_secret_access_key=AWSSECRETACCESSKEY)
-# clientAS  = boto3.client('autoscaling', region_name="us-east-1", aws_access_key_id=AWSACCESSKEYID, aws_secret_access_key=AWSSECRETACCESSKEY)
-
-# WAITER_TERMINATE_NV = client_nv.get_waiter('instance_terminated')
-# WAITER_RUNNING_NV = client_nv.get_waiter('instance_status_ok')
-# WAITER_IMAGE_NV = client_nv.get_waiter('image_available')
-
-# WAITER_TERMINATE_OH = client_oh.get_waiter('instance_terminated')
-# WAITER_RUNNING_OH = client_oh.get_waiter('instance_status_ok')
-
 def delete_existing_instances(client, OWNER_NAME, WAITER_TERMINATE):
     #EXISTTING INSTANCES
     try:
@@ -352,7 +262,6 @@ def delete_loadbalancers(client_lb, OWNER_NAME):
             print("Response:", response["ResponseMetadata"]["HTTPStatusCode"])
             print("Load Balancer(s) deletado(s)")
 
-
 def create_target_group(client_lb, client_ec2, targetGroupName):
     print('Criando Target Group...')
 
@@ -504,34 +413,6 @@ def delete_listener(client, OWNER_NAME):
                 return
     except:
         print("Algo de errado aconteceu na remoção do listener ou não há load balancer =^(  ")
-#------------------------------------------------------------------#
-
-# delete_existing_instances(client_nv, OWNER_NAME_NV, WAITER_TERMINATE_NV)
-# delete_existing_instances(client_oh, OWNER_NAME_OH, WAITER_TERMINATE_OH)
-
-# delete_auto_scaling_group(clientAS, AUTOSCALE_NAME)
-# LB_ARN = delete_loadbalancers(clientLB, OWNER_NAME)
-# delete_listener(clientLB, LB_ARN)
-# delete_images(client_nv)
-# delete_target_group(clientLB, TARGETGROUP_NAME)
-# delete_launch_configuration(clientAS, LAUNCH_NAME)
-
-# delete_credentials(client_nv, KEY_PAIR_NAME_NV, SEC_GROUP_NAME_NV)
-# delete_credentials(client_oh, KEY_PAIR_NAME_OH, SEC_GROUP_NAME_DB)
-# delete_credentials(client_nv, KEY_PAIR_NAME_NV, SEC_GROUP_NAME_LB)
-
-# SEC_GROUP_ID_NV = create_credentials(client_nv, KEY_PAIR_NAME_NV, SEC_GROUP_NAME_NV, PERMISSION_DJ)
-# SEC_GROUP_ID_DB = create_credentials(client_oh, KEY_PAIR_NAME_OH, SEC_GROUP_NAME_DB, PERMISSION_DB)
-# SEC_GROUP_ID_LB = create_credentials(client_nv, KEY_PAIR_NAME_NV, SEC_GROUP_NAME_LB, PERMISSION_LB)
-
-# POSTGRES_ID, POSTGRES_IP = create_db(client_oh, OWNER_NAME_OH, UBUNTU_OH, SEC_GROUP_ID_DB, SEC_GROUP_NAME_DB, KEY_PAIR_NAME_OH, WAITER_RUNNING_OH)
-# DJANGO_ID, DJANGO_IP = create_wb(client_nv, OWNER_NAME_NV, UBUNTU_NV, SEC_GROUP_ID_NV, SEC_GROUP_NAME_NV, KEY_PAIR_NAME_NV, WAITER_RUNNING_NV, POSTGRES_IP)
-# AMI_ID = create_ami(client_nv, OWNER_NAME_NV, DJANGO_ID, WAITER_IMAGE_NV)
-# TG_ARN = create_target_group(clientLB, client_nv, TARGETGROUP_NAME)
-# LB_ARN = create_loadbalancer(client_nv, clientLB, OWNER_NAME, SEC_GROUP_ID_LB)
-# create_launch_configuration(clientAS, LAUNCH_NAME, AMI_ID, SEC_GROUP_ID_NV)
-# create_auto_scaling_group(client_nv, clientAS, AUTOSCALE_NAME, LAUNCH_NAME, TG_ARN)
-# create_listener(clientLB, LB_ARN, TG_ARN)
 
 def deploy():
     with open('credentials.json') as fd:
